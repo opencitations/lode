b535cb484cd56182c0e057e3f08dc0fcf53f041d
Valentina <valentina.pasqual@studio.unibo.it>
2026-01-21 10:49:22 +0100

    refactor reader code

--- scripts/tests/test_example.py
+++
@@ -1,9 +0,0 @@
-"""Example tests for lode."""
-
-from lode import example_function
-
-
-def test_example_function():
-    """Test that example_function returns expected value."""
-    result = example_function()
-    assert result == "Hello from lode!"
