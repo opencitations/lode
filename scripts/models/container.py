e373d0ecb752d0e0ec28911f7909365def023e21
Valentina <valentina.pasqual@studio.unibo.it>
2026-01-30 15:36:55 +0100

    fix: model container

--- scripts/models/container.py
+++ scripts/models/container.py
@@ -5,12 +5,12 @@ class Container(Resource):
     
     def __init__(self):
         super().__init__()
-        self._members = []
+        self.members = []
     
     def set_has_member(self, member):
         """Aggiunge un singolo membro"""
         if member not in self._members:
-            self._members.append(member)
+            self.members.append(member)
     
     def set_has_members(self, members: list):
         """Imposta tutti i membri in una volta"""
@@ -18,4 +18,4 @@ class Container(Resource):
     
     def get_has_members(self):
         """Ritorna la lista dei membri"""
-        return self._members.copy()
+        return self.members.copy()
