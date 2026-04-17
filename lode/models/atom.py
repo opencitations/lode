2559aa7d0598dfc4f19c8ad1b19964b5d6911510
Valentina <valentina.pasqual@studio.unibo.it>
2026-04-17 17:02:11 +0200

    fix: rules model

--- lode/models/atom.py
+++ lode/models/atom.py
@@ -3,15 +3,12 @@ from .resource import Resource
 class Atom(Resource):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.has_argument1 = None   # Variable or Resource [1]
-        self.has_argument2 = None   # Variable, Resource (Individual or Literal (optional) per owl?) [0...1]
+        self.has_arguments = []   # Variable or Resource [1...n]
         self.has_predicate = None   # Concept (classPredicate) or Property (propertyPredicate) [1]
 
-    def get_has_argument1(self): return self.has_argument1
-    def set_has_argument1(self, v): self.has_argument1 = v
 
-    def get_has_argument2(self): return self.has_argument2
-    def set_has_argument2(self, v): self.has_argument2 = v
+    def get_has_arguments(self): return list(self.has_arguments)
+    def set_has_arguments(self, v): self.has_arguments.append(v)
 
     def get_has_predicate(self): return self.has_predicate
     def set_has_predicate(self, v): self.has_predicate = v
--- lode/models/rule.py
+++ lode/models/rule.py
@@ -3,11 +3,11 @@ from .resource import Resource
 class Rule(Resource):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.has_body = []  # Atom [1...*]
-        self.has_head = []  # Atom [1...*]
+        self.has_antecedent = []  # Atom [1..*]
+        self.has_consequent = []  # Atom [1..*]
 
-    def get_has_body(self): return list(self.has_body)
-    def set_has_body(self, atom): self.has_body.append(atom)
+    def get_has_antecedent(self): return list(self.has_antecedent)
+    def set_has_antecedent(self, atom): self.has_antecedent.append(atom)
 
-    def get_has_head(self): return list(self.has_head)
-    def set_has_head(self, atom): self.has_head.append(atom)
+    def get_has_consequent(self): return list(self.has_consequent)
+    def set_has_consequent(self, atom): self.has_consequent.append(atom)
